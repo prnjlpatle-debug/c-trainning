b816d0e2cee7c001c814c674a9172d9736e46695
prnjlpatle <prnjlpatle@gmail.com>
2025-09-03 13:08:23 +0530

    msg

---
+++ week1/c++/c1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{int a,b,temp;
+ a=5;
+ b=9;
+ temp=a;
+ a=b;
+ b=temp;
+ cout<<"value after swapping"<<a<<"and"<<b;
+ return 0;
+}
---
+++ week1/palindrome.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    int num=123;
+    int res;
+    int n=num;
+    system("cls");
+    while (num!=0)
+{
+    int lastdigit=num%10;
+    res= res*10+ lastdigit;
+    num=num/10;
+}
+if(res==n)
+{
+    printf("palindrome");
+}
+else
+{
+    printf("not a palindrome");
+}
+}
